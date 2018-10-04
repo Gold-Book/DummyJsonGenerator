76486c70037b8fd6314ffe695c81321534a2f8f5
GoldBook <masayakanemotoaws@gmail.com>
2018-10-04 19:17:19 +0900

    Cooperation localstack

--- container/shell/local.sh
+++ container/shell/local.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 env | sort
+
+waitforit -address=http://localstack/test:4572 -status=404 -timeout=30
+aws --endpoint-url=http://localstack:4572 s3api create-bucket --bucket test
+
 sam local start-api --docker-volume-basedir "${APPLICATION}/dist" --host 0.0.0.0 --template application/template.yaml
